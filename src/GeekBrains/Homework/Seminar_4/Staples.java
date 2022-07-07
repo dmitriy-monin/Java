e8f1302d76e362086e212f067761df55221b6cde
Dmitriy <xburv@mail.ru>
2022-07-07 17:01:40 +0500

    Java

--- src/GeekBrains/Homework/Seminar_4/Staples.java
+++ src/GeekBrains/Homework/Seminar_4/Staples.java
@@ -1,62 +1,63 @@
 package GeekBrains.Homework.Seminar_4;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.Scanner;
-import java.util.Stack;
 
 public class Staples {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
         System.out.print("Введите выражение: ");
-        String str = scan.nextLine();
+        String s = scan.nextLine();
 
-        Stack<Character> stack = new Stack<>();
-        boolean stat;
-        stat = true;
+        Deque<Character> deque = new ArrayDeque<>();
+        boolean status;
+        status = true;
 
-            for (int i = 0; i < str.length(); i++) {
-                switch (str.charAt(i)) {
+            for (int i = 0; i < s.length(); i++) {
+                switch (s.charAt(i)) {
                     case '(':
                     case '<':
                     case '[':
                     case '{':
-                        stack.push(str.charAt(i));
+                        deque.addFirst(s.charAt(i));
                         break;
 
                     case ')':
-                        if ((stack.peek() != '(') || stack.empty()) {
-                            stat = false;
+                        if ((deque.peekFirst() != '(') || deque.peekFirst() == null) {
+                            status = false;
                             break;
-                        } else stack.pop();
+                        } else deque.pop();
                         break;
 
                     case ']':
-                        if ((stack.peek() != '[') || stack.empty()) {
-                            stat = false;
+                        if ((deque.peekFirst() != '[') || deque.peekFirst() == null) {
+                            status = false;
                             break;
-                        } else stack.pop();
+                        } else deque.pop();
                         break;
 
                     case '}':
-                        if ((stack.peek() != '{') || stack.empty()) {
-                            stat = false;
+                        if ((deque.peekFirst() != '{') || deque.peekFirst() == null) {
+                            status = false;
                             break;
-                        } else stack.pop();
+                        } else deque.pop();
                         break;
 
                     case '>':
-                        if ((stack.peek() != '<') || stack.empty()) {
-                            stat = false;
+                        if ((deque.peekFirst() != '<') || deque.peekFirst() == null) {
+                            status = false;
                             break;
-                        } else stack.pop();
+                        } else deque.pop();
                         break;
                 }
-                if (!stat) {
+                if (!status) {
                     break;
                 }
             }
 
-            if (stack.empty())
+            if (deque.peekFirst() == null)
                 System.out.print(true);
             else
                 System.out.print(false);
