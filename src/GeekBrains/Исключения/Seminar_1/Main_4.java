08a3d4bdf718393f09a4cbadf43aec5eec30ab8a
Dmitriy <xburv@mail.ru>
2022-11-11 20:34:58 +0500

    Java

---
+++ src/GeekBrains/Исключения/Seminar_1/Main_4.java
@@ -0,0 +1,19 @@
+package GeekBrains.Исключения.Seminar_1;
+
+import java.util.ArrayList;
+
+public class Main_4 {
+    public static void main(String[] args) {
+        Integer[] arr = {1, 2, 3, null, 4, 7, null, null, 2};
+        System.out.println("Indexes with null values" + checkArray(arr));
+    }
+
+    public static ArrayList<Integer> checkArray(Integer[] arr) {
+        ArrayList<Integer> indexes = new ArrayList<Integer>();
+        for (int i = 0; i < arr.length; i++)
+            if (arr[i] == null) {
+                indexes.add(i);
+            }
+                return indexes;
+    }
+}
