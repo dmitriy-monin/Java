c01b131509bfd11ea000ef25f7200c5a6f97030a
Dmitriy <xburv@mail.ru>
2022-11-15 02:32:11 +0500

    Java

---
+++ src/GeekBrains/Исключения/Seminar_2/Homework/Homework_3.java
@@ -0,0 +1,27 @@
+package GeekBrains.Исключения.Seminar_2.Homework;
+
+public class Homework_3 {
+    public static void main(String[] args) {
+        try {
+            int a = 90;
+            int b = 3;
+            System.out.println(a / b);
+            printSum(23, 234);
+            int[] abc = {1, 2};
+            abc[0] = 9;
+        } catch (ArithmeticException ex) {
+            System.out.println("На ноль делить нельзя!");
+        } catch (NullPointerException ex) {
+            System.out.println("Указатель не может указывать на null!");
+        } catch (IndexOutOfBoundsException ex) {
+            System.out.println("Массив выходит за пределы своего размера!");
+        }  catch (Exception ex) {
+            System.out.println("Что-то пошло не так...");
+        }
+    }
+
+    public static void printSum(Integer a, Integer b) {
+        System.out.println(a + b);
+    }
+
+}
